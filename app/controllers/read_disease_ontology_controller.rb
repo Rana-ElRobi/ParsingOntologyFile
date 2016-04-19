c34b159b1c443d44df08aa1de093b9bf70f41626
rana <rana.saad.abdelhalim@fcis.asu.edu.eg>
2016-04-19 21:26:00 +0200

    changed teh structure of is_a from array to hash table , save ids of terms found in But saving ids id still Null

--- app/controllers/read_disease_ontology_controller.rb
+++ app/controllers/read_disease_ontology_controller.rb
@@ -1,12 +1,22 @@
 class ReadDiseaseOntologyController < ApplicationController
+  #Function that gets all names in is_a property
+  #Open disease ontology file
+  #check each line if contains "Is_a"
+  #if yes :  parse name part after '!'
+  #Check if it is already added in the names list
+  #if not : Push to the array	
   def is_a
-  	@is_a = []
+  	@is_a = Hash.new("" => [])
+
   	File.open("/home/rana/ParsingOntologyFile/HumanDO.obo", "r") do |f|
       f.each_line do |line|
-      	if line.include? "is_a"
+      	if line.include? "id"
+      		id = line.split(/: */)[2]
+
+      	elsif line.include? "is_a"
       		property = line.split(/! */)[1]
-      		if ! @is_a.include? property
-  	  			@is_a.push(property)
+      		if ! @is_a.has_key?(property)  
+  	  			@is_a[property] = id
   	  		end
   	  	end
   	  end
