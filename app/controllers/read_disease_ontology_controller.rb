8c809ef0b8f2dd4b6719762f4cc33dbd51ed3e10
rana <rana.saad.abdelhalim@fcis.asu.edu.eg>
2016-04-25 22:46:23 +0200

    write the Function that Gets all IDs in all properties of human ontology

--- app/controllers/read_disease_ontology_controller.rb
+++ app/controllers/read_disease_ontology_controller.rb
@@ -57,47 +57,43 @@ class ReadDiseaseOntologyController < ApplicationController
 
   	#@TypeDef = Hash.new { |property, key| property[key] = [] }
     @TypeDef = {transmitted_by: [] , results_in_formation_of: [] , results_in:  [] , realized_by_supression_with: [] , realized_by: [] , part_of:  [] , occurs_with:  [] , located_in:  [] , is_a: {} }
-  	File.open("/home/rana/ParsingOntologyFile/HumanDO.obo", "r") do |f|
+  	#Open ontology file
+    File.open("/home/rana/ParsingOntologyFile/HumanDO.obo", "r") do |f|
+      #loop on each line
       f.each_line do |line|
-        
-        #loop on all properties in m searching for 
-        @TypeDef.each do |property|
-          if line.include? "[Term]"
+        # => Clear current ID & current is_A type
+        if line.include? "[Term]"
             latest_id = ""
             latest_is_a = ""
-          end #end if
-
-          #find ID of the current term
-          if line.include? "id: DOID:"
-            latest_id = line.split(" ")[1]
-          end #end if of id 
-
-          #if the typeDef key is "is_a"
-          if property[:key] = "is_a"
-
-
-          #all proprties in Def 
-          else
-            if (line.include? "def: ") and (line.include? property)
-              property[:key] << latest_id
-            end # End if of def 
-          
-
-          end #end checking type of property
-              
-
-        end #end loop on hash of TypeDef	
-        
-         
-        
-
-        
-
-      	
-      		
-	end
-	# File is closed automatically at end of block
-  end
-
-end
-end
+        end #end if
+        # => Check if line has "ID :"
+        # => # => Take current ID
+        if line.include? "id: DOID:"
+          latest_id = line.split(" ")[1]
+        end #end id if
+        # => check if line starts with "is_a:"
+        # => # => get the type of is_a 
+        if line.include? "is_a: DOID:"
+          latest_is_a = line.split(" ! ")[1]
+          # => # => check if is_a type is not added before to is_a keys
+          # => # => # => if yes : add key & id
+          # => # => # => if not : Add id only to existing one
+          if !latest_id.empty? and !latest_is_a.empty?
+          @is_a[latest_is_a] << latest_id
+          end#end  check key & id
+        end #End is_a Contition
+        # => elsif check if line starts with "def:"
+        elsif (line.include? "def: ")
+          # => # => Loop on all deftypes 
+          @TypeDef.each do |property|
+            # => # => # => check if current defType included in def line 
+            if line.include? property
+            # => # => # => # => add current ID to current deftype [value]
+              @TypeDef[property] << latest_id
+            end #end property cheking
+          end #end loop on type def
+        end #end Def line
+      end #End file loop
+    end #end read file 
+  end #end def  
+end #end class controller
