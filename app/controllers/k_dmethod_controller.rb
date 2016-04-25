cba88624719dc5431437ad06ac050a95a4447ffb
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2016-04-26 01:39:18 +0200

    Fix again

--- app/controllers/k_dmethod_controller.rb
+++ app/controllers/k_dmethod_controller.rb
@@ -4,7 +4,7 @@ class KDmethodController < ApplicationController
   #loops on given sequence and puts each caharacter and its corresponding value in new hash 
   def seq_hydrophobicity (seq = "ACGTACGTACGTGGGGAAATTTTCCC")
     @k_d = Hash.new 
-  	@seq_hydrophob = Hash.new 
+  	@seq_hydrophob = []
   	File.open("#{Rails.root}/k_D Method.txt", "r") do |f|
   		#loop on each line
     	f.each_line do |line|
@@ -14,7 +14,7 @@ class KDmethodController < ApplicationController
 
     	#LOOP ON input seq
     	seq.split("").each do |char|
-    		@seq_hydrophob[char] = @k_d[char]
+    		@seq_hydrophob << [char, @k_d[char]]
     	end #end loop on sequence
 
 
