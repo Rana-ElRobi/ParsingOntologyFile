f02b42f2f2631fc857216f250f8bb50117a76214
rana <rana.saad.abdelhalim@fcis.asu.edu.eg>
2016-04-26 00:25:43 +0200

    write code of  the sequence hydrophobicity function in k_D method and it's view

--- app/controllers/k_dmethod_controller.rb
+++ app/controllers/k_dmethod_controller.rb
@@ -2,6 +2,23 @@ class KDmethodController < ApplicationController
   #Function calculate the hydrophobicity of given AA sequence
   #Read From file the AA and it's degree of hydrophobicity
   #loops on given sequence and puts each caharacter and its corresponding value in new hash 
-  def seq_hydrophobicity
-  end
+  def seq_hydrophobicity (seq = "ACGTACGTACGTGGGGAAATTTTCCC")
+  	@K_D = Hash.new 
+  	@seq_hydrophob = Hash.new 
+  	File.open("/home/rana/ParsingOntologyFile/k_D Method.txt", "r") do |f|
+  		#loop on each line
+    	f.each_line do |line|
+    		AA = line.split(" , ")[0]
+    		Degree = line.split(" , ")[1]
+    		@K_D[AA] << Degree
+    	end #end loop on lines
+
+    	#LOOP ON input seq
+    	seq.each do |char|
+    		@seq_hydrophob[char] >> K_D[char]
+    	end #end loop on sequence
+
+
+  	end #end read file
+  end #end Def
 end
