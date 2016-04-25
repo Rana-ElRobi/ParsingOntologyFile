ef56618be4b18349d7de1a20aeb2e962c7318693
rana <rana.saad.abdelhalim@fcis.asu.edu.eg>
2016-04-26 00:52:47 +0200

    edited in the function and the view of reading k-d file and calculating the given input sequence hydrophobicity

--- app/controllers/k_dmethod_controller.rb
+++ app/controllers/k_dmethod_controller.rb
@@ -8,14 +8,14 @@ class KDmethodController < ApplicationController
   	File.open("/home/rana/ParsingOntologyFile/k_D Method.txt", "r") do |f|
   		#loop on each line
     	f.each_line do |line|
-    		AA = line.split(" , ")[0]
-    		Degree = line.split(" , ")[1]
-    		@K_D[AA] << Degree
+    		aa = line.split(" , ")[0]
+        degree = line.split(" , ")[1]
+    		@K_D[:aa] = degree
     	end #end loop on lines
 
     	#LOOP ON input seq
-    	seq.each do |char|
-    		@seq_hydrophob[char] >> K_D[char]
+    	seq.split("").each do |char|
+    		@seq_hydrophob[:char] = @K_D[:char]
     	end #end loop on sequence
 
 
